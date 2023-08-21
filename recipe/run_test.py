41dc2bc09a3231c4317eafcd271a1c6f54bed115
Serhii Kupriienko <skupriienko@anaconda.com>
2023-08-21 18:40:00 +0300

    Skip s390x

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,24 +1,26 @@
-import fastapi
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-from fastapi import dependencies
-from fastapi import middleware
-from fastapi import openapi
-from fastapi import security
+import platform
 import subprocess
 
+import fastapi
+from fastapi import FastAPI, dependencies, middleware, openapi, security
 
 subprocess.run(["pip", "check"])
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_main():
     return {"msg": "Hello World"}
 
-client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+# Skip tests on s390x because of missing httpx package
+if platform.machine() != "s390x":
+    from fastapi.testclient import TestClient
+
+    client = TestClient(app)
+
+    def test_read_main():
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"msg": "Hello World"}
